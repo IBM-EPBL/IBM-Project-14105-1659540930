b662344d87651956d4457fe20dd294be4ed32692
PrasanthBE <113701794+PrasanthBE@users.noreply.github.com>
2022-11-08 21:27:28 +0530

    Delete m1.py

--- m1.py
+++
@@ -1,9 +0,0 @@
-#pip install playsound
-from random import randrange
-from playsound import playsound
-
-while(True):
-	temp= random.randint(0,100)
-	humidity = random.randint(0,100)
-	if(temp > 40 and humidity >40):
-		playsound('alarm.wav')
